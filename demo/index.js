0155d2fe12618bc6cd7295e3c82a1115adc53eed
Andrew Dirksen <andrew@dirksen.com>
2016-11-23 14:06:20 -0800

    Remove cross origin request from demo.

--- demo/index.js
+++ demo/index.js
@@ -11,35 +11,37 @@
     div.addEventListener('click', drawImage);
 
     function drawImage(event) {
-        var img = new Image();
-        img.setAttribute('crossOrigin','anonymous');
+        var id = String(Math.random()).split('.')[1];
+        var canvas = document.createElement('canvas');
+        var ctx = canvas.getContext("2d");
 
-        img.onload = function() {
-            var id = String(Math.random()).split('.')[1];
-            var canvas = document.createElement('canvas');
-            var ctx = canvas.getContext("2d");
+        canvas.id = id;
+        canvas.width = width;
+        canvas.height = height;
+        canvas.className = 'canvas';
+        canvas.style.left = event.clientX - (width / 2) + 'px';
+        canvas.style.top = event.clientY - (height / 2) + 'px';
 
-            canvas.id = id;
-            canvas.width = width;
-            canvas.height = height;
-            canvas.className = 'canvas';
-            canvas.style.left = event.clientX - (width / 2) + 'px';
-            canvas.style.top = event.clientY - (height / 2) + 'px';
-
-            ctx.drawImage(img, 0, 0);
-            div.appendChild(canvas);
-
-            var options = {
-                element: canvas,
-                currentScale: 0.5,
-                minScale: 0.1,
-                maxScale: 5,
-                easing: 0.8
-            }
-
-            much.add(options);
+        // draw sample image
+        ctx.beginPath();
+        ctx.lineWidth = width / 30;
+        for (var i = -height; i < width; i+=width/10) {
+          ctx.moveTo(i, 0);
+          ctx.lineTo(i+height, height);
         }
-        img.src = 'http://lorempixel.com/' + width + '/' + height + '/';
+        ctx.stroke();
+
+        div.appendChild(canvas);
+
+        var options = {
+            element: canvas,
+            currentScale: 0.5,
+            minScale: 0.1,
+            maxScale: 5,
+            easing: 0.8
+        }
+
+        much.add(options);
     }
 
     var GitHub = document.createElement('GitHub');
